739c4caab88906fea996d437226dd0d0199bd957
tveijola <tommi.veijola@gmail.com>
2020-05-16 18:40:27 +0300

    Node drawing and connecting implemented

--- src/main.py
+++ src/main.py
@@ -1,14 +1,38 @@
 from graphics import *
+import math
 from node import Node
 
-def draw_node(coord=Point(0,0), win=None):
-    circle = Circle(coord, radius=5)
+def draw_node(n, win=None):
+    circle = Circle(Point(n.x, n.y), radius=n.r)
     circle.draw(win)
+    conn_parent(n, win)
 
-nodes = []
-rootNode = Node()
+def conn_parent(n, win=None):
+    if n.parent is not None:
+        alpha = math.atan((n.y - n.parent.y) / abs(n.x - n.parent.x))
+        if n.x < n.parent.x:
+            p1 = Point(n.x + n.r * math.cos(alpha), n.y - n.r * math.sin(alpha))
+            p2 = Point(n.parent.x - n.parent.r * math.cos(alpha), n.parent.y + n.parent.r * math.sin(alpha))
+        else:
+            p1 = Point(n.x - n.r * math.cos(alpha), n.y - n.r * math.sin(alpha))
+            p2 = Point(n.parent.x + n.parent.r * math.cos(alpha), n.parent.y + n.parent.r * math.sin(alpha))
+        line = Line(p1, p2)
+        line.draw(win)
 
-win = GraphWin(width=200, height=200)
-draw_node()
-win.getMouse()
 
+w, h = 1000, 1000
+
+rootNode = Node(w/2, 20, 20)
+nodes = [rootNode]
+# Create nodes
+for i in range(9):
+    node = nodes[i]
+    print("Creating children for node num: {} [depth {}]".format(i, node.depth))
+    nodes.append(node.create_left_child())
+    nodes.append(node.create_right_child())
+
+window = GraphWin(width=w, height=h)
+for node in nodes:
+    draw_node(node, window)
+
+window.getMouse()
--- src/node.py
+++ src/node.py
@@ -1,18 +1,33 @@
 class Node:
-    def __init__(self, parent=None):
+    def __init__(self, x, y, r, parent=None):
         self.parent = parent
+        if self.parent is None:
+            self.depth = 0
+        else:
+            self.depth = self.parent.depth + 1
         self.l_child = None
         self.r_child = None
+        self.x = x
+        self.y = y
+        self.r = r
 
         self.locked = False
         self.left_des_locked = False
         self.right_des_locked = False
 
     def create_left_child(self):
-        self.l_child = Node(parent=self)
+        des_x = self.x - 250/(self.depth*2 + 1)
+        des_y = self.y + 200/(self.depth + 1)
+        des_r = self.r - 2
+        self.l_child = Node(x=des_x, y=des_y, r=des_r, parent=self)
+        return self.l_child
 
     def create_right_child(self):
-        self.r_child = Node(parent=self)
+        des_x = self.x + 250 / (self.depth*2 + 1)
+        des_y = self.y + 200 / (self.depth + 1)
+        des_r = self.r - 2
+        self.r_child = Node(x=des_x, y=des_y, r=des_r, parent=self)
+        return self.r_child
 
     def lock(self):
         # Check locking condition
@@ -23,19 +38,25 @@ class Node:
         if not(ancestor_locked or self.left_des_locked or self.right_des_locked):
             self.locked = True
             self.inform_ancestors(locked=True)
+            print("Locked node")
+            return True
+        else:
+            print("Could not lock node")
+            return False
 
     def unlock(self):
         self.locked = False
         self.inform_ancestors(locked=False)
+        print("Unlocked Node")
 
     def check_ancestors(self):
         if self.parent is None:
             return self.locked
         else:
-            if self.locked:
-                return self.locked
+            if self.parent.locked:
+                return self.parent.locked
             else:
-                return self.check_ancestors()
+                return self.parent.check_ancestors()
 
     def inform_ancestors(self, locked=False):
         if self.parent is None:
@@ -49,3 +70,24 @@ class Node:
 
 if __name__ == '__main__':
     rootNode = Node()
+    nodes = [rootNode]
+    # Create nodes
+    for i in range(3):
+        node = nodes[i]
+        print("Creating children for node num: {} [depth {}]".format(i, node.depth))
+        nodes.append(node.create_left_child())
+        nodes.append(node.create_right_child())
+
+    for i in range(len(nodes)):
+        print("Node num: {} [depth {}]".format(i, nodes[i].depth))
+
+    nodes[0].lock()
+    nodes[4].lock()
+    nodes[1].lock()
+    nodes[3].lock()
+    nodes[2].lock()
+    nodes[0].unlock()
+    nodes[4].lock()
+    nodes[1].lock()
+    nodes[3].lock()
+    nodes[2].lock()
